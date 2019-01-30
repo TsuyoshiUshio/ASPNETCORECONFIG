c36439a7d47d5a774d6429576fd5558dd499e45f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2019-01-30 15:39:06 -0800

    Remove the level.

--- AspNetCoreSpike/Startup.cs
+++ AspNetCoreSpike/Startup.cs
@@ -27,7 +27,7 @@ namespace AspNetCoreSpike
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
+            services.Configure<ApplicationSettings>(Configuration);            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
